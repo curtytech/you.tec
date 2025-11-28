bf45ba9d7378f7d4b7ef4baba76741a060cb6438
Phelipe Curty <71573615+curtytech@users.noreply.github.com>
2025-11-28 08:55:44 -0300

    "Sobre"

--- src/components/Layout/Header/Navigation/menuData.tsx
+++ src/components/Layout/Header/Navigation/menuData.tsx
@@ -2,15 +2,15 @@ import { HeaderItem } from "@/types/menu";
 
 export const headerData: HeaderItem[] = [
   { label: "Home", href: "/" },
-  { label: "Sobre", href: "/about" },
+  // { label: "Sobre", href: "/about" },
   { label: "Serviços", href: "/services" },
   { label: "Portfolio", href: "/portfolio" },
-  {
-    label: "Blog",
-    href: "/blog",
-    submenu: [
-      { label: "Blog list", href: "/blog" },
-      { label: "Blog details", href: "/blog/Blog_1" },
-    ],
+  // {
+  //   label: "Blog",
+  //   href: "/blog",
+  //   submenu: [
+  //     { label: "Blog list", href: "/blog" },
+  //     { label: "Blog details", href: "/blog/Blog_1" },
+  //   ],
   },
 ];  
